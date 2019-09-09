412d12b23cf482087e5f0992a483b530dd9d8499
Nicholas Wu <nicholas.wu@nick.local>
2019-09-09 14:39:17 -0400

    Update

---
+++ WhatILike.py
@@ -0,0 +1,8 @@
+print ("I like food")
+print ("I like travel")
+print ("I like animals")
+print ("I like reading")
+print ("I like watching youtube")
+print ("I like listening to music")
+print ("I have a brother")
+print ("I have no pets :(")
