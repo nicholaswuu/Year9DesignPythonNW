b593bd1dd4a645cc0ce9e6b8ed2f52573b90a61a
Nicholas Wu <nicholas.wu@nick.local>
2019-09-17 12:07:57 -0400

    Exercise 1 - Console Work

--- Exercise 1 - Console Work/ConVolCylStep2.py
+++ Exercise 1 - Console Work/ConVolCylStep2.py
@@ -1,12 +1,17 @@
 import math
 
+print("\nThis program will find the volume by using the radius and height")
 print("\nVolume of a Cylinder Formula: ")
-print("\nVolume = \u03C0 \u00d7 radius\u00b2 \u00d7 height")
+print("\n\tLet r = radius and h = height")
+print("\n\tVolume = \u03C0r\u00b2h")
+
+
 #Input
 #What inputs are needed to calculate the volume of a cylinder?
 #Radius and height
 
-name = input("\nWhat is your name: ")
+print("\nInput: ")
+name = input("\n\tWhat is your name: ")
 
 radius = input("\n\tInput radius(cm): ")
 radius = (int)(radius)
@@ -18,8 +23,10 @@ height = (int)(height)
 #Process
 #What formula is used to calculate the volume of a cylinder?
 #Volume = pi * radius * radius * height
+
 volume = math.pi*math.pow(radius, 2)*height
 volume = round(volume, 2)
+
 
 #Output
 #What is important about the output?
@@ -28,4 +35,4 @@ print("\nHi "+name+"!")
 print("\nGiven a cylinder with:")
 print("\n\tRadius = "+str(radius))
 print("\n\tHeight = "+str(height))
-print("\nThe volume is: "+str(volume)+"\n")  
+print("\nThe volume is: "+str(volume)+"\n") 
---
+++ Exercise 1 - Console Work/ConVolCylStep3.py
@@ -0,0 +1,42 @@
+import math
+
+print("\nThis program will find the volume by using the radius and height")
+print("\nVolume of a Cylinder Formula: ")
+print("\n\tLet r = radius and h = height")
+print("\n\tVolume = \u03C0r\u00b2h")
+
+
+#Input
+#What inputs are needed to calculate the volume of a cylinder?
+#Radius and height
+
+print("\nInput: ")
+name = input("\n\tWhat is your name: ")
+
+radius = 1
+height = 1
+while (radius != 0 or height != 0):
+
+	radius = input("\n\tInput radius(cm): ")
+	radius = (int)(radius)
+
+	height = input("\n\tInput height(cm): ")
+	height = (int)(height)
+
+
+	#Process
+	#What formula is used to calculate the volume of a cylinder?
+	#Volume = pi * radius * radius * height
+
+	volume = math.pi*math.pow(radius, 2)*height
+	volume = round(volume, 2)
+
+
+	#Output
+	#What is important about the output?
+
+	print("\nHi "+name+"!")
+	print("\nGiven a cylinder with:")
+	print("\n\tRadius = "+str(radius))
+	print("\n\tHeight = "+str(height))
+	print("\nThe volume is: "+str(volume)+"\n") 
