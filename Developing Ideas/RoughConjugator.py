6400e4da0fd435d62f97bf4181e70b1d30675c8c
Nicholas Wu <nicholas.wu@GCCYJPNJJ1WK.local>
2019-10-30 00:33:58 -0400

    Conjugator very rough draft - regular avoir verbs only

--- Developing Ideas/RoughConjugator.py
+++ Developing Ideas/RoughConjugator.py
@@ -6,11 +6,25 @@ verb = input("Verb:")
 #Process and output:
 n = len(verb)
 
-if verb[n-2:n] == "er": #check if the verb is an -er verb
-	print(pronoun + " "+verb[:n-2]+"é") #if it is, output the conjugated version by taking away "er" and adding é
+
+if pronoun == "je" or "Je":	#Check what pronoun it is to determine how to conjugate the auxiliary
+	newpronoun = "J'ai"
+if pronoun == "tu":
+	newpronoun =  pronoun + " as"
+if pronoun == "il" or pronoun == "elle" or pronoun == "on":
+	newpronoun = pronoun + " a"
+if pronoun == "nous":
+	newpronoun = pronoun + " avons"
+if pronoun == "vous":
+	newpronoun = pronoun + " avez"
+if pronoun == "ils" or pronoun =="elles":
+	newpronoun = pronoun + " ont"
+
+if verb[n-2:n] == "er": #check if the verb is an -er verb and if the 
+	print(newpronoun + " "+verb[:n-2]+"é") #if it is, output the conjugated version by taking away "er" and adding é
 
 if verb[n-2:n] == "ir":	#check if the verb is an -ir verb
-	print(pronoun + " "+verb[:n-1]) #if it is, output the conjugated version by deleting the "r"
+	print(newpronoun + " "+verb[:n-1]) #if it is, output the conjugated version by deleting the "r"
 
 if verb[n-2:n] == "re":	#check if the verb is an -re verb
-	print(pronoun + " "+verb[:n-2]+"u") #if it is, output the conjugated version by taking away "re" and adding "u"
+	print(newpronoun + " "+verb[:n-2]+"u") #if it is, output the conjugated version by taking away "re" and adding "u"
