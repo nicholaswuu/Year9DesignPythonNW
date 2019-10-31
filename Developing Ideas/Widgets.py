39b37e9cfdf7c94498977d1a8c74bb20a3d81470
Nicholas Wu <nicholas.wu@GCCYJPNJJ1WK.local>
2019-10-31 00:11:03 -0400

    widgets sample code

--- Developing Ideas/Widgets.py
+++ Developing Ideas/Widgets.py
@@ -2,12 +2,21 @@ import tkinter as tk
 
 root = tk.Tk()
 
-button = tk.Button(root, text = "Conjugate")
-button.config(fg = "green", width = 25, height=2)
-button.pack()
+textbox = tk.Text(root)#Textbox to place outputs and examples.
+textbox.config(width = 100, height = 5)	
+textbox.pack()
+textbox.insert(tk.END, "Basic widgets that will be used in this program")
 
-verbEntry = tk.Entry(root)
+verbEntry = tk.Entry(root)	#Entry box, will be used for verb and pronoun
 verbEntry.config(width = 100)
 verbEntry.pack()
+
+button = tk.Button(root, text = "Conjugate")	#Button, will be used to do the conjugation, and for audio
+button.config(fg = "green", width = 25, height=2)
+button.pack(pady=30)
+
+check = tk.Checkbutton(root, text = "High Contrast")	#Checkbox for high contrast
+check.config(anchor = tk.W)
+check.pack(fill = tk.BOTH)
 
 root.mainloop()
