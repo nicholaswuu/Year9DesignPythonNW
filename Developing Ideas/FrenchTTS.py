073186e14ea260b8915e90661101e54fa04dfce0
Nicholas Wu <nicholas.wu@GCCYJPNJJ1WK.local>
2019-11-02 16:43:52 -0400

    French text to voice sample code

---
+++ Developing Ideas/FrenchTTS.py
@@ -0,0 +1,3 @@
+import os
+fword = input("French word: ")	#Take an input for a french word
+os.system('say -v Thomas ' + fword)	#Have Thomas (The french voice name for mac) say the word from terminal
