6ca92cf52e8eac5b81c7d8311a9fe0cb26dcedb7
Nicholas Wu <nicholas.wu@GCCYJPNJJ1WK.local>
2019-12-13 09:49:25 -0500

    Max and Min

--- Data Analysis/DataAnalysis1.py
+++ Data Analysis/DataAnalysis1.py
@@ -8,4 +8,46 @@ for i in range(0, len(dataList), 1):
 	dataList[i] = dataList[i].replace(",","")
 	dataList[i] = float(dataList[i])
 
-print(dataList)
+minimum = min(dataList)
+print(minimum)
+
+maximum = max(dataList)
+print(maximum)
+
+diff = maximum - minimum
+print(diff)
+
+smallest = dataList[0]
+for i in range (0, len(dataList), 1):
+	if smallest > dataList[i]:
+		smallest = dataList[i]
+
+print("MIN IS: " + str(smallest))
+
+largest = dataList[0]
+for i in range (0, len(dataList), 1):
+	if largest < dataList[i]:
+		largest = dataList[i]
+
+print("MAX IS: " + str(largest))
+
+
+maxvalue = input("What number do you want to set as upper limit: ")
+maxvalue = float(maxvalue)
+biggest = dataList[0]
+minvalue = input("What number do you want to set as lower limit: ")
+minvalue = float(minvalue)
+smallest1 = dataList[0]
+
+for i in range (0, len(dataList), 1):
+	if maxvalue > dataList[i] and dataList[i] > biggest:
+		biggest = dataList[i]
+
+print("MAX IS: " + str(biggest))
+
+for i in range (0, len(dataList), 1):
+	if minvalue < dataList[i] < smallest1:
+		smallest1 = dataList[i]
+
+print("MIN IS: " + str(smallest1))
+
---
+++ Technical Skills/WarmupSE.py
@@ -0,0 +1,8 @@
+list = [1,4,9,2]
+for i in range(0, len(list), 2):
+	print(list[i])
+
+
+str = input("String:")
+for i in range(0, len(str), 2):
+	print(str[i])
